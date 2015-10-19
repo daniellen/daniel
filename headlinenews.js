36f5793cc347a40eb0ad994b2c773502def89b91
daniellen <dk582425@gmail.com>
2015-10-20 06:18:02 +0700

    Update and rename README.md to headlinenews.js

---
+++ headlinenews.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+           var hn_url_blog = "http://satelit-iklan-ads.blogspot.com";
+           var hn_jumlah_post = 5;
+           var hn_warna_latar = "#EEEEEE";
+           var hn_warna_garis = "#000000";
+           var hn_posisi = "top";
+           var hn_tampilkan_judul = true;
+           var hn_backlink = true;
+          
+</script>
+           <script src="http://daniel.googlecode.com/files/headlinenews.js">
+</script>
